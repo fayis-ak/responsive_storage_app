d87925c1fe409708419264bcdce0a0fb49ee66db
fayis <fayisak456@gmail.com>
2023-09-27 18:45:43 +0530

    new drawer add review

---
+++ lib/drawerPages.dart/transaction.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Transactions extends StatelessWidget {
+  const Transactions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transaction'),
+      ),
+    );
+  }
+}
